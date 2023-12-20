3b12d5ce49ddf5b6c217caef4ea8e585d2ab9876
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2023-12-20 17:17:55 +0300

    Qo'shimcha ashiyo, taqqoslash va savatcha page qo'shildi

--- tailwindcss_starter_project/tailwind.config.js
+++ tailwindcss_starter_project/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html", "./abaut/Products.html", "./abaut/Smartfon.html"],
+    content: ["./index.html", "./abaut/Products.html", "./abaut/Smartfon.html", "./abaut/Savatcha.html", "./abaut/Taqqoslash.htm", "./abaut/AShiyo.htm"],
     theme: {
         extend: {},
     },
